fa4cc592fc7442c429900799b16cc4e1c81911b9
Alden Vivian <alden.vivian@revature.com>
2025-05-21 15:21:23 +0530

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  let name = "svelte";
+</script>
+
+<h1>Welcome to {name.toUpperCase()}</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
