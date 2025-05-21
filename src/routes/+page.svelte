50d34a7547f3093087ba9de195b6d0113dfa6b74
Alden Vivian <alden.vivian@revature.com>
2025-05-21 17:05:48 +0530

    delete

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  let name = "svelte";
-</script>
-
-<h1>Welcome to {name.toUpperCase()}</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
